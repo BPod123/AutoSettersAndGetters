a3238b5e524ccb7564fcab5284209f9be6ab3493
Ben Podrazhansky <benpodrazhansky@gmail.com>
2019-10-31 20:04:21 -0400

    Setters and getters.java

--- ModifierType.java
+++
@@ -1,7 +0,0 @@
-public enum ModifierType {
-    FINAL,
-    STATIC,
-    FINALSTATIC,
-    STATICFINAL,
-    DEFAULT;
-}
--- attribute/AccessType.java
+++
@@ -1,6 +0,0 @@
-public enum AccessType{
-    PUBLIC,
-    PRIVATE,
-    PROTECTED,
-    DEFAULT;
-}
